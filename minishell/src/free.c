03df64515befd27c626060bb5914f0e394970825
slecoq <stephanelecoq66@gmail.com>
2023-11-22 15:31:30 +0100

    a la norme

--- minishell/src/free.c
+++ minishell/src/free.c
@@ -6,7 +6,7 @@
 /*   By: slecoq <slecoq@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:50:55 by slecoq            #+#    #+#             */
-/*   Updated: 2023/11/20 14:10:09 by slecoq           ###   ########.fr       */
+/*   Updated: 2023/11/22 14:28:19 by slecoq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 void	ft_free(char **str)
 {
-	int i = -1;
+	int	i;
 
+	i = -1;
 	while (str[++i])
 		free(str[i]);
 	free(str);
 }
 
-void ft_free_lstvar(t_env *lst)
+void	ft_free_lstenv(t_env *lst)
 {
-	t_env *cur;
-	t_env *next;
+	t_env	*cur;
+	t_env	*next;
 
 	cur = lst;
 	while (cur)
@@ -37,23 +38,7 @@ void ft_free_lstvar(t_env *lst)
 	}
 }
 
-void ft_free_lstenv(t_env *lst)
-{
-	t_env *cur;
-	t_env *next;
-
-	cur = lst;
-	while (cur)
-	{
-		next = cur->next;
-		free(cur->name);
-		free(cur->value);
-		free(cur);
-		cur = next;
-	}
-}
-
-void ft_free_cell(t_env *lst)
+void	ft_free_cell(t_env *lst)
 {
 	free(lst->name);
 	free(lst->value);
